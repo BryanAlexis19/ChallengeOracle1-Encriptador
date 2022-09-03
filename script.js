775968ada74037e1a2260e8929119be5fa732011
BryanAlexis19 <librale19@gmail.com>
2022-09-02 21:26:18 -0500

    Challenge finished sucessfully

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-const inputIngreso = document.getElementById("txtIngreso");
-const mensajeEncriptado = document.getElementById("txtEncriptado");
+//Cajas Generales
+btnCopiar = document.getElementById("btnCopiar");
+imgMuneco = document.querySelector(".muneco");
+title1 = document.querySelector(".title-1");
+title2 = document.querySelector(".title-2");
+
+//Aparecer Caja de mensaje encriptado: display = flex;
+var inputIngreso = document.getElementById("txtIngreso");
+var mensajeEncriptado = document.getElementById("txtEncriptado");
 const btnEncriptation = document
   .getElementById("btnEncriptation")
   .addEventListener("click", function (e) {
     e.preventDefault();
   });
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
 
+const btnDesencriptation = document
+  .getElementById("btnDesencriptation")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+  });
+
+//Get the event when pressing Encriptar button
 const btnEncriptarHandler = (e) => {
-  console.log("hola");
-  const encrypted = encriptar(inputIngreso.value);
-  mensajeEncriptado.value = encrypted;
-  mensajeEncriptado.style.backgroundImage = "none";
-  inputIngreso.value = "";
-  console.log("final");
+  if (inputIngreso.value) {
+    const encrypted = encriptar(inputIngreso.value);
+    imgMuneco.style.display = "none";
+    title1.style.display = "none";
+    title2.style.display = "none";
+    mensajeEncriptado.value = encrypted;
+    mensajeEncriptado.style.display = "inline";
+    btnCopiar.style.display = "inline";
+    inputIngreso.value = "";
+  } else {
+    alert("Ingrese un texto a encriptar");
+    imgMuneco.style.display = "inline";
+    title1.style.display = "inline";
+    title2.style.display = "inline";
+    mensajeEncriptado.style.display = "none";
+    btnCopiar.style.display = "none";
+  }
 };
 
+//Function to crypt the entered message.
 const encriptar = (stringEncriptado) => {
   let arrayCodigo = [
     ["e", "enter"],
@@ -40,9 +61,67 @@ const encriptar = (stringEncriptado) => {
   return stringEncriptado;
 };
 
-const copyText = () => {
-    mensajeEncriptado.select();
-    navigator.clipboard.writeText(mensajeEncriptado.value)
-    mensajeEncriptado = "";
-    alert("Mensaje Copiado");
+//FUNCTION TO DECRYPT THE ENTERED MESSAGE
+const btnDesencriptarHandler = (e) => {
+  if (inputIngreso.value) {
+    const encrypted = desencriptar(inputIngreso.value);
+    imgMuneco.style.display = "none";
+    title1.style.display = "none";
+    title2.style.display = "none";
+    mensajeEncriptado.value = encrypted;
+    mensajeEncriptado.style.display = "inline";
+    btnCopiar.style.display = "inline";
+    inputIngreso.value = "";
+  }else{
+    alert("Ingrese un texto a desencriptar.");
+    imgMuneco.style.display = "inline";
+    title1.style.display = "inline";
+    title2.style.display = "inline";
+    mensajeEncriptado.style.display = "none";
+    btnCopiar.style.display = "none";
+  }
+};
+
+const desencriptar = (stringEncriptado) => {
+  let arrayCodigo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  stringEncriptado = stringEncriptado.toLowerCase();
+  for (let i = 0; i < arrayCodigo.length; i++) {
+    if (stringEncriptado.includes(arrayCodigo[i][1])) {
+      stringEncriptado = stringEncriptado.replaceAll(
+        arrayCodigo[i][1],
+        arrayCodigo[i][0]
+      );
+    }
+  }
+  return stringEncriptado;
+};
+
+
+//FUNCTION TO COPY THE GENERATED TEXT
+const btnCopiarHandler = (e) => {
+  mensajeEncriptado.select();
+  navigator.clipboard.writeText(mensajeEncriptado.value);
+  mensajeEncriptado.value = "";
+  alert("Mensaje Copiado");
+};
+
+
+//FUNCTION TO REVERSE ENCRYPTION
+const btnReverseHandler = (e) => {
+  if(mensajeEncriptado.value){
+    inputIngreso.value = mensajeEncriptado.value;
+    mensajeEncriptado.value = "";
+    //Empty result area
+    imgMuneco.style.display = "inline";
+    title1.style.display = "inline";
+    title2.style.display = "inline";
+    mensajeEncriptado.style.display = "none";
+    btnCopiar.style.display = "none";
+  }
 }
